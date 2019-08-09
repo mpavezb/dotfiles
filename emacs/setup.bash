3442726d31fdfdc972b25ceae0bc13e2f700a352
Matias Pavez <Matias.Bahamondes@partner.bmw.de>
2019-08-09 14:48:43 +0200

    echos in emacs setup.bash

--- emacs/setup.bash
+++ emacs/setup.bash
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo " - Installing emacs configuration into '$HOME/.emacs'."
+cp $DIR/emacs     $HOME/.emacs
+
+echo " - Creating '$HOME/.emacs.d/lisp/ directory."
 mkdir -p $HOME/.emacs.d/lisp
 
-cp $DIR/emacs     $HOME/.emacs
+echo " - Copying lisp scripts into .emacs.d/"
 cp $DIR/lisp  -r  $HOME/.emacs.d/
 
+echo "OK"
