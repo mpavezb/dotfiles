5c7496b34e8671e37581bf1542ce764379347174
matias pavez <matias.pavez.b@gmail.com>
2019-09-29 23:00:15 +0200

    emacs isearch, hippie completion and allow scroll on search

--- emacs/settings/editor.el
+++ emacs/settings/editor.el
@@ -30,6 +30,62 @@
 ;; (desktop-read)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; misc
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; store backups inside user-emacs-directory.
+(setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups"))))
+
+;; remember point position when reopening/revisiting each buffer
+(require 'saveplace)
+(setq-default save-place t)
+(setq save-place-file (concat user-emacs-directory "places"))
+
+;; highlight parenthesis
+(show-paren-mode 1)
+
+;; show row,column numbers
+(setq column-number-mode t)
+
+;; auto reload files from disk
+(global-auto-revert-mode t)
+
+;; force newline at EOF
+;;(setq require-final-newline t)
+
+;; prefer spaces over tabs
+;;(setq-default indent-tabs-mode nil)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Kill-Yank for Clipboard and Mouse
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Killing and yanking uses the X clipboard rather than just the primary selection.
+(setq save-interprogram-paste-before-kill t)
+
+; after copy Ctrl+c in Linux X11, you can paste by yanking in emacs.
+(setq select-enable-clipboard t)
+
+; Mouse yanking inserts at the point instead of the location of the click.
+(setq mouse-yank-at-point t)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ibuffer
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(autoload 'ibuffer "ibuffer" "List buffers." t)
+
+;; nearly all of this is the default layout
+(setq ibuffer-formats 
+      '((mark modified read-only " "
+              (name 50 50 :left :elide) " "
+	      (size 9 -1 :right) " "
+              (mode 16 16 :left :elide) " " filename-and-process)
+        (mark " " (name 16 -1) " " filename)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Reopen killed buffers
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; keep a stack of killed buffers (only for files)
@@ -67,51 +123,26 @@ Emacs session."
           (find-file file)))
     (error "No recently-killed files to reopen")))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; store backups inside user-emacs-directory.
-(setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups"))))
-
-;; remember point position when reopening/revisiting each buffer
-(require 'saveplace)
-(setq-default save-place t)
-(setq save-place-file (concat user-emacs-directory "places"))
-
-;; highlight parenthesis
-(show-paren-mode 1)
-
-;; show row,column numbers
-(setq column-number-mode t)
-
-;; auto reload files from disk
-(global-auto-revert-mode t)
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Kill-Yank for Clipboard and Mouse
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; Killing and yanking uses the X clipboard rather than just the primary selection.
-(setq save-interprogram-paste-before-kill t)
-
-; after copy Ctrl+c in Linux X11, you can paste by yanking in emacs.
-(setq select-enable-clipboard t)
-
-; Mouse yanking inserts at the point instead of the location of the click.
-(setq mouse-yank-at-point t)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ibuffer
+;; search
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-(autoload 'ibuffer "ibuffer" "List buffers." t)
 
-;; nearly all of this is the default layout
-(setq ibuffer-formats 
-      '((mark modified read-only " "
-              (name 50 50 :left :elide) " "
-	      (size 9 -1 :right) " "
-              (mode 16 16 :left :elide) " " filename-and-process)
-        (mark " " (name 16 -1) " " filename)))
+;; REGEX search
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
+(setq isearch-allow-scroll t)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; completion
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; hippie-expand
+;; Allows expansion for autocompletion (given an interesting source)
+;; https://www.emacswiki.org/emacs/HippieExpand
+(global-set-key (kbd "M-/") 'hippie-expand)
