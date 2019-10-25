adb0de389b58f6c681293f58f6ab1a658fb94366
matias pavez <matias.pavez.b@gmail.com>
2019-10-25 21:39:15 +0200

    bash: remove buggy terminal title name
    Causes text wrap when rewritting commands.

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -151,9 +151,5 @@ __prompt_command() {
     PS1+="$(__format_bold "]")"
     PS1+="$(__prompt_git)"
     PS1+=" $(__prompt_symbol) "
-
-    # change terminal title to show current directory
-    PS1+="\[\e]2; \"${PWD/${HOME}/\~}\"\a \]"
 }
 PROMPT_COMMAND=__prompt_command
-
