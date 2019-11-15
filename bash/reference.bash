8ad667da458624812c2c79fb677ce5e7aa32f62c
Matias Pavez <Matias.Bahamondes@partner.bmw.de>
2019-11-15 09:32:42 +0100

    update bash reference

--- bash/reference.bash
+++ bash/reference.bash
@@ -4,3 +4,17 @@
 # notify when command finishes
 # (defined in .bashrc)
 echo "hello world"; alert
+
+# =============================================================================
+# Redirection
+# ==============================================================================
+
+# to file
+echo "hello world" > file.log 2>&1
+
+# to file and console
+echo "hello world" 2>&1 | tee file.log
+
+# pipe multiple commands to one
+{ echo "hello"; echo "world"; } | nl
+cat <(echo "hello") <(echo "world") | nl
