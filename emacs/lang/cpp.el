d555ea1ef084da5d4c97019cf16f7169f292c179
matias pavez <matias.pavez.b@gmail.com>
2019-11-17 04:01:29 +0100

    emacs: cpp use-package flags

--- emacs/lang/cpp.el
+++ emacs/lang/cpp.el
@@ -5,12 +5,11 @@
 ;; -----------------------------------------------------------------------------
 ;; (sudo apt install clang-format)
 (use-package clang-format+ 
-  :diminish clang-format+-mode)
+  :diminish clang-format+-mode
 
-;; format buffer on save
-(add-hook 'c-mode-common-hook #'clang-format+-mode)
-(setq clang-format+-context 'buffer)
-
+  ;; format buffer on save
+  :hook (c-mode-common . clang-format+-mode) 
+  :config (setq clang-format+-context 'buffer))
 
 ;; -----------------------------------------------------------------------------
 ;; Syntax Highlighting
@@ -19,14 +18,13 @@
 ;; syntax highlighting for modern cpp until C++20
 ;; https://github.com/ludwigpacifici/modern-cpp-font-lock
 (use-package modern-cpp-font-lock 
-  :diminish modern-c++-font-lock-mode)
-(modern-c++-font-lock-global-mode t)
+  :diminish modern-c++-font-lock-mode 
+  :config (modern-c++-font-lock-global-mode t))
 
 ;; -----------------------------------------------------------------------------
 ;; Refactoring
 ;; -----------------------------------------------------------------------------
-(use-package srefactor)
-(semantic-mode 1)
-
+(use-package srefactor 
+  :config (semantic-mode 1))
 (define-key c-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
 (define-key c++-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
