c5a6196c198e1cf70de9ec1951dfbed15404fff5
Matias Pavez <Matias.Bahamondes@partner.bmw.de>
2020-01-13 14:24:43 +0100

    [emacs] do not format emacs var/ files.

--- emacs/lang/lisp.el
+++ emacs/lang/lisp.el
@@ -9,9 +9,17 @@
   (add-to-list 'elisp-format-split-subexp-keyword-except-list '"use-package"))
 
 ;; format on save
+;; only applied to files
 (defun mp/lisp-mode-before-save-hook () 
-  (when (eq major-mode 'emacs-lisp-mode) 
-    (elisp-format-buffer)))
+  (if (string-prefix-p (expand-file-name (concat user-emacs-directory "var/")) 
+		       (expand-file-name (or (buffer-file-name) 
+					     ""))) 
+      (message (concat "elisp-format: File is in .emacs.d/var/ directory. Will not apply hook: " (or
+												  (buffer-file-name) 
+												  "<buffer is not a file>"))) 
+    (when (eq major-mode 'emacs-lisp-mode) 
+      (elisp-format-buffer))))
+
 (add-hook  'before-save-hook 'mp/lisp-mode-before-save-hook)
 
 ;; use this to disable the format hook
