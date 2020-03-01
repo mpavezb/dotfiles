eaefe888fb23804abe3820e73e59f2acbcdc857b
matias pavez <matias.pavez.b@gmail.com>
2020-03-02 00:22:35 +0100

    [bash] Add fzf preview for C-t / M-c

--- bash/fzf/fzf.bash
+++ bash/fzf/fzf.bash
@@ -1,13 +1,58 @@
 #!/bin/bash
-FZF_DIR="${DOTFILES}"/bash/fzf/
+# Set up FZF Command Line Fuzzy Finder Utility
+# - Load
+# - Default Settings
+# - Settings for default bindings
+# - Bindings for git, bazel, ...
+#
+# References:
+# - https://github.com/junegunn/fzf#environment-variables
+# - man fzf
+#
 
-# fzf file exists
+# ===============================================
+# Loading
+# ===============================================
+# No fzf file
 [[ ! -f ~/.fzf.bash ]] && return
 
-# only if not in emacs
+# Don't load if running inside emacs
 [[ ! -z ${INSIDE_EMACS+x} ]] && return
 
+# Load
 source ~/.fzf.bash
+
+# ===============================================
+# Utils
+# ===============================================
+FZF_DIR="${DOTFILES}"/bash/fzf/
+source "${FZF_DIR}"/utils.bash
+
+# ===============================================
+# Default Settings
+# ===============================================
+FZF_DEFAULT_COMMAND=
+FZF_DEFAULT_OPTS=
+export FZF_DEFAULT_COMMAND
+export FZF_DEFAULT_OPTS
+
+# ===============================================
+# Default Bindings
+# ===============================================
+FZF_PREVIEW_OPTS="--preview='mp::fzf_preview_command {}' --preview-window='right:50%'"
+
+FZF_CTRL_T_OPTS="${FZF_PREVIEW_OPTS}"
+FZF_CTRL_R_OPTS=
+FZF_ALT_C_OPTS="${FZF_PREVIEW_OPTS}"
+
+export FZF_CTRL_T_OPTS
+export FZF_CTRL_R_OPTS
+export FZF_ALT_C_OPTS
+unset FZF_PREVIEW_OPTS
+
+# ===============================================
+# Git
+# ===============================================
 source "${FZF_DIR}"/git.bash
-source "${FZF_DIR}"/bazel.bash
-source "${FZF_DIR}"/complete.bash
+
+unset FZF_DIR
---
+++ bash/fzf/utils.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function mp::fzf_preview_file
+{
+    local path=$1
+
+    [[ ! -f $path ]] && return
+
+    if which bat >/dev/null; then
+	bat --style=numbers --color=always "${path}" | head -100
+    else
+	head -100 "${path}"
+    fi
+}
+
+function mp::fzf_preview_command
+{
+    local path=$1
+    if [[ -d $path ]]; then
+	echo -e "\e[1mDirectory:\e[0m"
+	echo "$(readlink -f ${path})"
+	echo ""
+	echo -e "\e[1mContents:\e[0m"
+	find "${path}" | head -100
+    elif [[ -f $path ]]; then
+	mp::fzf_preview_file "${path}"
+    else
+	echo "Path ${path} os not a file or directory!"
+    fi
+}
+export -f mp::fzf_preview_file
+export -f mp::fzf_preview_command
