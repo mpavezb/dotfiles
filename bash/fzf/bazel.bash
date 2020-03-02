42b162578ccad3a7f1c4f2b11d09f0b1d32b1eb4
matias pavez <matias.pavez.b@gmail.com>
2020-03-02 02:47:50 +0100

    [bash] FZF bazel autocompletion

--- bash/fzf/bazel.bash
+++ bash/fzf/bazel.bash
@@ -1,7 +1,44 @@
 #!/bin/bash
+# Provides FZF completion for bazel targets
+# Completion is triggered by //.*<TAB>.
+#
+# See: https://github.com/junegunn/fzf/wiki/Examples-(completion)
 
-mp::bazel_targets() {
-    bazel query ... |
-    fzf --height 40% --ansi --no-sort --reverse
+# ===============================================
+# Default Bazel Completion
+# ===============================================
+if [ -f /usr/local/lib/bazel/bin/bazel-complete.bash ]; then
+    source /usr/local/lib/bazel/bin/bazel-complete.bash
+fi
+
+# ===============================================
+# Exit if bazel is not found
+# ===============================================
+if ! which bazel >/dev/null; then
+    return
+fi
+[[ ! $(complete -p | grep _bazel__complete) ]] && return
+
+# ===============================================
+# FZF bazel autocompletion
+# ===============================================
+# Use FZF completion if current word starts with "//".
+# Use _bazel__complete otherwise.
+
+_fzf_complete_bazel() {
+    local cur
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    if [[ ${cur} =~ ^// ]]; then
+	_fzf_complete "--multi --reverse" "$@" < <(
+	    bazel query ... 2> /dev/null
+	)
+    else
+	_bazel__complete
+    fi
 }
-bind '"\C-b\C-t": "$(mp::bazel_targets)\e\C-e\er"'
+
+_fzf_complete_bazel_notrigger() {
+    FZF_COMPLETION_TRIGGER='' _fzf_complete_bazel
+}
+
+complete -F _fzf_complete_bazel_notrigger -o nospace -o default -o bashdefault bazel
