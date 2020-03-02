bb263057ebab4256b231ff8ccd339545d5ec21b2
Matias Pavez <matias.bahamondes@partner.bmw.de>
2020-03-02 17:54:38 +0100

    [bash] fzf use fd instead of find

--- bash/fzf/fzf.bash
+++ bash/fzf/fzf.bash
@@ -27,10 +27,29 @@ FZF_DIR="${DOTFILES}"/bash/fzf
 source "${FZF_DIR}"/utils.bash
 
 # ===============================================
-# Default Settings
+# Commands
 # ===============================================
-
+# Use fd as search engine (faster than find)
+# https://github.com/sharkdp/fd
+#
 FZF_DEFAULT_COMMAND=
+FZF_CTRL_T_COMMAND=
+FZF_CTRL_R_COMMAND=
+FZF_ALT_C_COMMAND=
+if which fd > /dev/null; then
+    COMMON_OPTS=" --color=never --hidden --follow --exclude .git"
+    FZF_DEFAULT_COMMAND="fd --type f ${COMMON_OPTS} ."
+    FZF_CTRL_T_COMMAND="fd --type f ${COMMON_OPTS} ."
+    FZF_ALT_C_COMMAND="fd --type d ${COMMON_OPTS} ."
+    unset COMMON_OPTS
+fi
+export FZF_DEFAULT_COMMAND
+export FZF_ALT_C_COMMAND
+
+# ===============================================
+# Options
+# ===============================================
+FZF_PREVIEW_OPTS="--preview='mp::fzf_preview_command {}' --preview-window='right:50%'"
 
 # Use JellyX Scheme
 # https://github.com/junegunn/fzf/wiki/Color-schemes
@@ -39,18 +58,11 @@ FZF_DEFAULT_OPTS='
   --color info:150,prompt:110,spinner:150,pointer:167,marker:174
   --color preview-bg:234
 '
-export FZF_DEFAULT_COMMAND
-export FZF_DEFAULT_OPTS
-
-# ===============================================
-# Default Bindings
-# ===============================================
-FZF_PREVIEW_OPTS="--preview='mp::fzf_preview_command {}' --preview-window='right:50%'"
-
 FZF_CTRL_T_OPTS="--height 95% --multi ${FZF_PREVIEW_OPTS}"
 FZF_CTRL_R_OPTS="--height 50% --border"
 FZF_ALT_C_OPTS="--height 95% ${FZF_PREVIEW_OPTS}"
 
+export FZF_DEFAULT_OPTS
 export FZF_CTRL_T_OPTS
 export FZF_CTRL_R_OPTS
 export FZF_ALT_C_OPTS
