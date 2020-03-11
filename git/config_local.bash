87932764231f05aad36a7433833556b97875035a
Matias Pavez <matias.bahamondes@partner.bmw.de>
2020-03-11 14:05:18 +0100

    [git] Add local gitconfig setup script

---
+++ git/config_local.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd "${DIR}"
+git config --local user.name "Matias Pavez"
+git config --local user.email "matias.pavez.b@gmail.com"
+git config --local credential.helper "cache --timeout=864000"
+git config --list --local
+
