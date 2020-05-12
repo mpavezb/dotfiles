c48f2d0528de4391b1f899cf95b2253361955f2b
Matias Pavez <matias.pavez.b@gmail.com>
2020-05-13 00:50:44 +0200

    [emacs] Remove automatic https selector for package-archives

--- emacs/system/packages.el
+++ emacs/system/packages.el
@@ -11,20 +11,6 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
-;; (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos)) 
-;; 		    (not (gnutls-available-p)))) 
-;;        (proto (if no-ssl "http" "https")))
-;;   ;; load from MELPA
-;;   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-
-;;   ;; load from MELPA-STABLE
-;;   (add-to-list 'package-archives (cons "melpa-stable" (concat proto
-;; 							      "://stable.melpa.org/packages/")) t)
-
-;;   ;; load from GNU ELPA for older versions of emacs.
-;;   (when (< emacs-major-version 24) 
-;;     (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
-
 (package-initialize)
 
 ;; prevents stale elisp bytecode from shadowing more up-to-date source files.
