0f1f0dd294093bfb24c1ef6ea93231d03270a347
Matias Pavez <matias.pavez.b@gmail.com>
2020-05-23 16:24:34 +0200

    [emacs] Add python indent-tools

--- emacs/lang/python.el
+++ emacs/lang/python.el
@@ -2,6 +2,15 @@
 (setq python-indent-guess-indent-offset-verbose nil)
 
 ;; -----------------------------------------------------------------------------
+;; indent-tools
+;; Indent,  move around  and  act  on code  based  on indentation.
+;; https://gitlab.com/emacs-stuff/indent-tools/
+;; -----------------------------------------------------------------------------
+(add-hook 'python-mode-hook ;;
+	  (lambda () 
+	    (define-key python-mode-map (kbd "C-c >") 'indent-tools-hydra/body)))
+
+;; -----------------------------------------------------------------------------
 ;; anaconda mode
 ;; Code navigation, documentation lookup and completion for Python.
 ;; https://github.com/pythonic-emacs/anaconda-mode
