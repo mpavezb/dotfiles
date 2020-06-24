9d2ed4d4a3edf05251e4b79d98d7559c0b42f3d3
Matias Pavez <matias.pavez.b@gmail.com>
2020-06-25 01:35:56 +0200

    [bash] Add FZF themes

--- bash/fzf/fzf.bash
+++ bash/fzf/fzf.bash
@@ -47,20 +47,40 @@ export FZF_DEFAULT_COMMAND
 export FZF_ALT_C_COMMAND
 
 # ===============================================
+# THEMES
+# https://github.com/junegunn/fzf/wiki/Color-schemes
+# ===============================================
+
+mp_fzf_theme_jellyx() {
+    FZF_DEFAULT_OPTS='
+	--color fg:-1,bg:-1,hl:230,fg+:3,bg+:233,hl+:229
+        --color info:150,prompt:110,spinner:150,pointer:167,marker:174
+        --color preview-bg:234'
+}
+
+mp_fzf_theme_solarized() {
+    FZF_DEFAULT_OPTS='
+	--color=bg+:#073642,bg:#002b36,spinner:#719e07,hl:#586e75
+  	--color=fg:#839496,header:#586e75,info:#cb4b16,pointer:#719e07
+	--color=marker:#719e07,fg+:#839496,prompt:#719e07,hl+:#719e07'
+}
+
+mp_fzf_theme_red() {
+    FZF_DEFAULT_OPTS='
+	--color fg:124,bg:16,hl:202,fg+:214,bg+:52,hl+:231
+	--color info:52,prompt:196,spinner:208,pointer:196,marker:208'
+}
+
+# default theme
+mp_fzf_theme_jellyx
+
+# ===============================================
 # Options
 # ===============================================
 FZF_PREVIEW_OPTS=
 FZF_PREVIEW_OPTS="${FZF_PREVIEW_OPTS} --preview='_mp-fzf_preview_command {}'"
 FZF_PREVIEW_OPTS="${FZF_PREVIEW_OPTS} --preview-window='right:50%'"
 FZF_PREVIEW_OPTS="${FZF_PREVIEW_OPTS} --bind pgdn:preview-page-down,pgup:preview-page-up"
-
-# Use JellyX Scheme
-# https://github.com/junegunn/fzf/wiki/Color-schemes
-FZF_DEFAULT_OPTS='
-  --color fg:-1,bg:-1,hl:230,fg+:3,bg+:233,hl+:229
-  --color info:150,prompt:110,spinner:150,pointer:167,marker:174
-  --color preview-bg:234
-'
 
 FZF_CTRL_T_OPTS="--height 95% --multi ${FZF_PREVIEW_OPTS}"
 FZF_CTRL_R_OPTS="--height 50% --border"
