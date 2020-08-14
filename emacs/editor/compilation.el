a2c30ed720dbf44640f2a2264ed377d9cc8ed4d0
Matias Pavez <matias.pavez.b@gmail.com>
2020-08-14 15:29:02 +0200

    [emacs] Add compilation mode settings

--- emacs/editor/compilation.el
+++ emacs/editor/compilation.el
@@ -1,5 +1,33 @@
-;; Set behavior of compilation-next-error
-;; 2 -- skip anything less than error,
-;; 1 -- skip anything less than warning or
-;; 0 -- don't skip any messages.
+
+;; This gives a regular `compile-command' prompt.
+(define-key prog-mode-map [C-f9] #'compile)
+
+;; This just compiles immediately.
+(define-key prog-mode-map [f9] #'mp/compile-please)
+
+;; Dont ask about saving buffers
+(setq compilation-ask-about-save nil)
+
+;; Stop on the first error.
+(setq compilation-scroll-output 'next-error)
+
+;; Ignore info and warnings.
 (setq compilation-skip-threshold 2)
+
+(defcustom mp/compile-window-size 105 
+  "Width given to the non-compilation window." 
+  :type 'integer 
+  :group 'mp)
+
+(defun mp/compile-please (comint) 
+  "Compile without confirmation. With a prefix argument, use comint-mode." 
+  (interactive "P")
+
+  ;; Do the command without a prompt.
+  (save-window-excursion (compile (eval compile-command) 
+				  (and comint 
+				       t)))
+
+  ;; Create a compile window of the desired width.
+  (pop-to-buffer (get-buffer "*compilation*"))
+  (enlarge-window (- (frame-width) mp/compile-window-size (window-width)) 'horizontal))
