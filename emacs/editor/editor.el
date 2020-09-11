625e3ca1f2659d83afb10141a6411290aac11283
Matias Pavez <matias.pavez.b@gmail.com>
2020-09-11 18:14:22 +0200

    [emacs] Add FZF fork

--- emacs/editor/editor.el
+++ emacs/editor/editor.el
@@ -96,8 +96,10 @@
 ;; fzf
 ;; Emacs frontend to FZF
 ;; https://github.com/bling/fzf.el
+;; https://github.com/mpavezb/fzf.el (fork)
 ;; https://github.com/junegunn/fzf
 ;; -----------------------------------------------------------------------------
+(load-file (expand-file-name (concat user-emacs-directory "third_party/" "fzf.el/fzf.el")))
 (use-package fzf 
   :bind (("C-c f f" . fzf) 
 	 ("C-c f p" . fzf-projectile) 
