cd029f8bee580bf3f19ed1e9bc005c1952c8a618
Matias Pavez <matias.pavez.b@gmail.com>
2020-09-19 00:33:39 +0200

    [emacs] hippie-expand to not complete lines

--- emacs/lang/ide.el
+++ emacs/lang/ide.el
@@ -22,6 +22,13 @@
 ;; https://www.emacswiki.org/emacs/HippieExpand
 ;; -----------------------------------------------------------------------------
 ;; Use hippie-expand as fallback
+(setq hippie-expand-try-functions-list '(try-expand-dabbrev try-expand-dabbrev-all-buffers
+							    try-expand-dabbrev-from-kill
+							    try-complete-file-name-partially
+							    try-complete-file-name
+							    try-expand-all-abbrevs
+							    try-complete-lisp-symbol-partially
+							    try-complete-lisp-symbol))
 (global-set-key (kbd "M-/") 'hippie-expand)
 
 ;; -----------------------------------------------------------------------------
