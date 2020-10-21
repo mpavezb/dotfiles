a03329a06812eae04edeb2bececc0485a04635a8
Matias Pavez <matias.pavez.b@gmail.com>
2020-10-21 15:30:33 +0200

    [bash] Use fzf/bazel completion when native completion is absent

--- bash/fzf/bazel.bash
+++ bash/fzf/bazel.bash
@@ -7,28 +7,24 @@
 # ===============================================
 # Exit if bazel is not found
 # ===============================================
-if ! which bazel >/dev/null; then
+if ! hash bazel 2>/dev/null; then
     return
 fi
 
 # ===============================================
 # Default Bazel Completion
 # ===============================================
-# if no completion is found, then load default one.
+# load default bazel completion if none is found.
 if ! complete -p | grep -q 'bazel'; then
     if [ -f /usr/local/lib/bazel/bin/bazel-complete.bash ]; then
 	source /usr/local/lib/bazel/bin/bazel-complete.bash
-    else
-	# There is no bazel completion at this point.
-	# Nothing to do about it.
-	return
     fi
 fi
 
 # ===============================================
 # FZF bazel autocompletion
 # ===============================================
-# Use FZF completion if current word starts with "//".
+# Use FZF completion for bazel commands where current word starts with "//".
 # Use _bazel__complete otherwise.
 
 _fzf_complete_bazel() {
@@ -38,7 +34,7 @@ _fzf_complete_bazel() {
 	_fzf_complete "--multi --reverse" "$@" < <(
 	    bazel query ... 2> /dev/null
 	)
-    else
+    elif hash _bazel__complete 2>/dev/null; then
 	_bazel__complete
     fi
 }
