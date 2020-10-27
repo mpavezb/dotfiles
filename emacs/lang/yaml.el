7513ce21671ed0c2bcaa0ee1b66ef7c8170bfb80
Matias Pavez <matias.pavez.b@gmail.com>
2020-10-27 12:30:12 +0100

    [emacs] Cleanup yaml-mode

--- emacs/lang/yaml.el
+++ emacs/lang/yaml.el
@@ -6,20 +6,6 @@
   ;; Allow using ENTER for newline and indent
   (add-hook 'yaml-mode-hook '(lambda () 
 			       (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
-
-  ;; Enable line numbers
-  (add-hook 'yaml-mode-hook 'linum-mode)
   ;;
   )
 
-;; -----------------------------------------------------------------------------
-;; indent-tools
-;; Indent,  move around  and  act  on code  based  on indentation.
-;; https://gitlab.com/emacs-stuff/indent-tools/
-;; -----------------------------------------------------------------------------
-(use-package indent-tools 
-  :after yaml-mode 
-  :config ;;
-  (global-set-key (kbd "C-c >") 'indent-tools-hydra/body)
-  ;;
-  )
