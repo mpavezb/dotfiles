972ef8fe6040cb9dd81a25dcea44df68d9e92bce
Matias Pavez <matias.pavez.b@gmail.com>
2020-12-10 20:06:58 -0300

    [emacs] use fzf fork with straight

--- emacs/pkgs/fzf.el
+++ emacs/pkgs/fzf.el
@@ -1,12 +1,14 @@
 ;; -----------------------------------------------------------------------------
 ;; fzf
 ;; Emacs frontend to FZF
-;; https://github.com/bling/fzf.el
+;; https://github.com/bling/fzf.el (original)
 ;; https://github.com/mpavezb/fzf.el (fork)
-;; https://github.com/junegunn/fzf
+;; https://github.com/junegunn/fzf (tool)
 ;; -----------------------------------------------------------------------------
-(load-file (expand-file-name (concat user-emacs-directory "third_party/" "fzf.el/fzf.el")))
 (use-package fzf
+   :straight (fzf :type git :host github :repo "bling/fzf.el"
+                      :fork (:host github
+				   :repo "mpavezb/fzf.el"))
   :commands (fzf fzf-projectile fzf-directory)
   :config ;;
   (setq fzf/args "-x --no-hscroll --margin=0,1,1,0 --print-query")
