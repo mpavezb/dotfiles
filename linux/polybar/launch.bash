900e935c343e91512630fc9892e5e239e2b78851
Matias Pavez <matias.pavez.b@gmail.com>
2021-05-07 18:56:00 +0200

    [i3] Launch updated polybar

--- linux/polybar/launch.bash
+++ linux/polybar/launch.bash
@@ -6,15 +6,15 @@ killall -q polybar
 # Wait Until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bars
+# Use this in case the monitor names are flaky
+# export MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
+
+# Launch multiple bars
 # echo "---" | tee -a /tmp/polybar-top.log /tmp/polybar-bottom.log
 # polybar top    2>&1 | tee -a /tmp/polybar-top.log    & disown
 # polybar bottom 2>&1 | tee -a /tmp/polybar-bottom.log & disown
 
-# Use this in case the monitor names are flaky
-# export MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
-
 echo "---" | tee -a /tmp/polybar-top.log
-polybar top 2>&1 | tee -a /tmp/polybar.log & disown
+polybar main 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Bars launched..."
